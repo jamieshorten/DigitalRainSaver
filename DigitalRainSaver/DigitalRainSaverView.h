c41e798244c74b3dcc7aeeff7423686dcbfb4d3b
Jamie Shorten <jamie@jamieshorten.com>
2023-02-28 20:57:35 +0000

    Initial Commit

---
+++ DigitalRainSaver/DigitalRainSaverView.h
@@ -0,0 +1,12 @@
+//
+//  DigitalRainSaverView.h
+//  DigitalRainSaver
+//
+//  Created by Jamie Shorten on 28/02/2023.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface DigitalRainSaverView : ScreenSaverView
+
+@end
---
+++ DigitalRainSaver/DigitalRainSaverView.m
@@ -0,0 +1,51 @@
+//
+//  DigitalRainSaverView.m
+//  DigitalRainSaver
+//
+//  Created by Jamie Shorten on 28/02/2023.
+//
+
+#import "DigitalRainSaverView.h"
+
+@implementation DigitalRainSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
